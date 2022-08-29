8d90372c80ec53836797a9afd422e7f99a271583
Cookiez <insanecookiez22@gmail.com>
2022-08-29 13:57:17 +0200

    Event callbacks added

--- Argus/src/Argus/ImGui/ImGuiLayer.h
+++ Argus/src/Argus/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Argus/Layer.h"
 
+#include "Argus/Events/ApplicationEvent.h"
+#include "Argus/Events/KeyEvent.h"
+#include "Argus/Events/MouseEvent.h"
+
 namespace Argus
 {
 	class ARGUS_API ImGuiLayer : public Layer
@@ -16,6 +20,15 @@ namespace Argus
 		void OnEvent(Event& event);
 
 	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 		float m_Time = 0.f;
 	};
 }
