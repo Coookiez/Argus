aee6fa7b3d413e0ef8e0b65f9c88a78ca9af5aa5
Cookiez <insanecookiez22@gmail.com>
2022-10-02 20:21:37 +0200

    Added basic transform component

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,12 @@
 #include <Argus.h>
 
+#include "glm/gtc/matrix_transform.hpp"
 
 class ExampleLayer : public Argus::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
 	{
 		///  TRIANGLE
 
@@ -36,10 +37,10 @@ public:
 		///  SQUARE
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			+0.75f, -0.75f, 0.0f,
-			+0.75f, +0.75f, 0.0f,
-			-0.75f, +0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			+0.5f, -0.5f, 0.0f,
+			+0.5f, +0.5f, 0.0f,
+			-0.5f, +0.5f, 0.0f
 		};
 
 		std::shared_ptr<Argus::VertexBuffer> squareVB;
@@ -61,6 +62,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 			
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -69,7 +71,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -95,13 +97,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 			
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -127,9 +130,9 @@ public:
 	{
 		//AS_TRACE("Delta time: {0}s ({1}ms)", ts.GetSeconds(), ts.GetMilliseconds());
 
-		if (Argus::Input::IsKeyPressed(AS_KEY_LEFT))
+		if (Argus::Input::IsKeyPressed(AS_KEY_RIGHT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
-		else if (Argus::Input::IsKeyPressed(AS_KEY_RIGHT))
+		else if (Argus::Input::IsKeyPressed(AS_KEY_LEFT))
 			m_CameraPosition.x += m_CameraMoveSpeed * ts;
 
 		if (Argus::Input::IsKeyPressed(AS_KEY_UP))
@@ -142,6 +145,16 @@ public:
 		else if (Argus::Input::IsKeyPressed(AS_KEY_D))
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 
+		if (Argus::Input::IsKeyPressed(AS_KEY_J))
+			m_SquarePosition.x -= m_CameraMoveSpeed * ts;
+		else if (Argus::Input::IsKeyPressed(AS_KEY_L))
+			m_SquarePosition.x += m_CameraMoveSpeed * ts;
+
+		if (Argus::Input::IsKeyPressed(AS_KEY_K))
+			m_SquarePosition.y -= m_CameraMoveSpeed * ts;
+		else if (Argus::Input::IsKeyPressed(AS_KEY_I))
+			m_SquarePosition.y += m_CameraMoveSpeed * ts;
+
 
 		Argus::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Argus::RenderCommand::Clear();
@@ -151,8 +164,18 @@ public:
 
 		Argus::Renderer::BeginScene(m_Camera);
 
-		Argus::Renderer::Submit(m_BlueShader, m_SquareVA);
-		Argus::Renderer::Submit(m_Shader, m_VertexArray);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 10; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Argus::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+		//Argus::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Argus::Renderer::EndScene();
 	}
@@ -174,6 +197,8 @@ private:
 	float m_CameraMoveSpeed = 1.f;
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 100.f;
+
+	glm::vec3 m_SquarePosition;
 
 };
 
