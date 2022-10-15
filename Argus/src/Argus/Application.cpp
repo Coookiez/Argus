8bcc2bdb44192ea8e8332c1a95f4da30c8db3158
Cookiez <insanecookiez22@gmail.com>
2022-10-15 19:26:44 +0200

    Resize is now handled properly

--- Argus/src/Argus/Application.cpp
+++ Argus/src/Argus/Application.cpp
@@ -35,6 +35,7 @@ namespace Argus {
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(OnWindowResize));
 		//AS_CORE_TRACE("{0}", e);
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
@@ -65,8 +66,11 @@ namespace Argus {
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
 
-			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate(timestep);
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+			}
 
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
@@ -83,4 +87,17 @@ namespace Argus {
 		return true;
 	}
 
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+		
+		m_Minimized = false;
+		Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+		return false;
+	}
 }
--- Argus/src/Argus/Renderer/RenderCommand.cpp
+++ Argus/src/Argus/Renderer/RenderCommand.cpp
@@ -1,10 +1,9 @@
 #include "aspch.h"
-#include "RenderCommand.h"
+#include "Argus/Renderer/RenderCommand.h"
 
 #include "Platform/OpenGL/OpenGLRendererAPI.h"
 
 namespace Argus
 {
 	RendererAPI* RenderCommand::s_RendererAPI = new OpenGLRendererAPI;
-
 }
