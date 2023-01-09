0ab30ea893c917956b36a9b1ad1aee3ecedc7819
Cookiez <insanecookiez22@gmail.com>
2023-01-09 13:17:43 +0100

    Camera Component UI added

--- Argus/src/Argus/Scene/SceneCamera.cpp
+++ Argus/src/Argus/Scene/SceneCamera.cpp
@@ -12,12 +12,22 @@ namespace Argus
 
 	void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
 	{
+		m_ProjectionType = ProjectionType::Orthographic;
 		m_OrthographicSize = size;
 		m_OrthographicNear = nearClip;
 		m_OrthographicFar = farClip;
 		RecalculateProjection();
 	}
 	
+	void SceneCamera::SetPerspective(float verticalFOV, float nearClip, float farClip)
+	{
+		m_ProjectionType = ProjectionType::Perspective;
+		m_PerspectiveFOV = verticalFOV;
+		m_PerspectiveNear = nearClip;
+		m_PerspectiveFar = farClip;
+		RecalculateProjection();
+	}
+
 	void SceneCamera::SetViewportSize(uint32_t width, uint32_t height)
 	{
 		m_AspectRatio = (float)width / (float)height;
@@ -26,11 +36,18 @@ namespace Argus
 
 	void SceneCamera::RecalculateProjection()
 	{
-		float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
-		float orthoRight = -orthoLeft;
-		float orthoBottom = -m_OrthographicSize * 0.5f;
-		float orthoTop = -orthoBottom;
+		if (m_ProjectionType == ProjectionType::Perspective)
+		{
+			m_Projection = glm::perspective(m_PerspectiveFOV, m_AspectRatio, m_PerspectiveNear, m_PerspectiveFar);
+		}
+		else
+		{
+			float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
+			float orthoRight = -orthoLeft;
+			float orthoBottom = -m_OrthographicSize * 0.5f;
+			float orthoTop = -orthoBottom;
 
-		m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+			m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+		}
 	}
 }
