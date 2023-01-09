dcb43128745d10fb4209bfaf51b540618531eb2d
Cookiez <insanecookiez22@gmail.com>
2023-01-10 00:07:07 +0100

    Adding / removing entities and components added

--- Argus/src/Argus/Scene/Scene.h
+++ Argus/src/Argus/Scene/Scene.h
@@ -15,6 +15,7 @@ namespace Argus
 		~Scene();
 
 		Entity CreateEntity(const std::string& name = std::string());
+		void DestroyEntity(Entity entity);
 
 		// TEMP
 		entt::registry& Reg() { return m_Registry; }
@@ -22,6 +23,9 @@ namespace Argus
 		void OnUpdate(Timestep ts);
 		void OnViewportResize(uint32_t width, uint32_t height);
 	private:
+		template<typename T>
+		void OnComponentAdded(Entity entity, T& component);
+
 		entt::registry m_Registry;
 		uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
