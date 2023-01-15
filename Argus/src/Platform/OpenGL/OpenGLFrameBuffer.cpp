9805bc40cf3ca93b72587ca01b8a908085ec2bb0
Cookiez <insanecookiez22@gmail.com>
2023-01-15 19:52:24 +0100

    Clearing texture attachments

--- Argus/src/Platform/OpenGL/OpenGLFrameBuffer.cpp
+++ Argus/src/Platform/OpenGL/OpenGLFrameBuffer.cpp
@@ -25,16 +25,16 @@ namespace Argus
 			glBindTexture(TextureTarget(mutlisampled), id);
 		}
 
-		static void AttachColorTexture(uint32_t id, int samples, GLenum format, uint32_t width, uint32_t height, int index)
+		static void AttachColorTexture(uint32_t id, int samples, GLenum internalFormat, GLenum format, uint32_t width, uint32_t height, int index)
 		{
 			bool multisampled = samples > 1;
 			if (multisampled)
 			{
-				glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, format, width, height, GL_FALSE);
+				glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, internalFormat, width, height, GL_FALSE);
 			}
 			else
 			{
-				glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+				glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, nullptr);
 
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -74,6 +74,20 @@ namespace Argus
 				case FrameBufferTextureFormat::DEPTH24STENCIL8:	return true;
 			}
 			return false;
+		}
+
+		static GLenum ArgusFBTextureFormatToGL(FrameBufferTextureFormat format)
+		{
+			switch (format)
+			{
+				case Argus::FrameBufferTextureFormat::RGBA8:
+					return GL_RGBA8;
+				case Argus::FrameBufferTextureFormat::RED_INTEGER:
+					return GL_RED_INTEGER;
+			}
+
+			AS_CORE_ASSERT(false);
+			return 0;
 		}
 	}
 
@@ -128,7 +142,10 @@ namespace Argus
 				switch (m_ColorAttachmentSpecifications[i].TextureFormat)
 				{
 					case FrameBufferTextureFormat::RGBA8:
-						Utils::AttachColorTexture(m_ColorAttachments[i], m_Specification.Samples, GL_RGBA8, m_Specification.Width, m_Specification.Height, i);
+						Utils::AttachColorTexture(m_ColorAttachments[i], m_Specification.Samples, GL_RGBA8, GL_RGBA, m_Specification.Width, m_Specification.Height, i);
+						break;
+					case FrameBufferTextureFormat::RED_INTEGER:
+						Utils::AttachColorTexture(m_ColorAttachments[i], m_Specification.Samples, GL_R32I, GL_RED_INTEGER, m_Specification.Width, m_Specification.Height, i);
 						break;
 				}
 			}
@@ -167,7 +184,7 @@ namespace Argus
 	{
 		glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
 		glViewport(0, 0, m_Specification.Width, m_Specification.Height);
-	}
+			}
 
 	void OpenGLFrameBuffer::Unbind()
 	{
@@ -188,4 +205,21 @@ namespace Argus
 		Invalidate();
 	}
 
+	int OpenGLFrameBuffer::ReadPixel(uint32_t attachmentIndex, int x, int y)
+	{
+		AS_CORE_ASSERT(attachmentIndex < m_ColorAttachments.size());
+
+		glReadBuffer(GL_COLOR_ATTACHMENT0 + attachmentIndex);
+		int pixelData;
+		glReadPixels(x, y, 1, 1, GL_RED_INTEGER, GL_INT, &pixelData);
+		return pixelData;
+	}
+
+	void OpenGLFrameBuffer::ClearAttachment(uint32_t attachmentIndex, int value)
+	{
+		AS_CORE_ASSERT(attachmentIndex < m_ColorAttachments.size());
+		
+		auto& spec = m_ColorAttachmentSpecifications[attachmentIndex];
+		glClearTexImage(m_ColorAttachments[attachmentIndex], 0, Utils::ArgusFBTextureFormatToGL(spec.TextureFormat), GL_INT, &value);
+	}
 }
